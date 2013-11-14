82cf052aec13330ef023c5caa8bc2d6db943ff0a
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-11-14 21:43:29 +0900

    Projects 바인딩 완료.

--- app/uploaders/project_big_uploader.rb
+++ app/uploaders/project_big_uploader.rb
@@ -17,12 +17,12 @@ class ProjectBigUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  
+    "/img/Rockwell_Norman-Going_and_Coming.jpg"
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
